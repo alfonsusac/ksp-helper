8e5c786138c9a218123a89c26e452b710b09795a
Alfonsus Ardani <alfonsusac@gmail.com>
2026-07-18 20:05:22 +0700

    break word

--- src/ui/list.tsx
+++ src/ui/list.tsx
@@ -6,7 +6,7 @@ export function CitationList(props: {
   return (
     <li>
       <span className="text-slate-700">{props.title}</span>{props.author ? <span className="text-slate-500"> by {props.author}</span> : ""}<br />
-      <a className="text-slate-400/75 underline underline-offset-3 decoration-slate-300 hover:text-slate-400 hover:decoration-slate-400" href={props.href} target="_blank">{props.href}</a>
+      <a className="text-slate-400/75 underline underline-offset-3 decoration-slate-300 hover:text-slate-400 hover:decoration-slate-400 break-all" href={props.href} target="_blank">{props.href}</a>
     </li>
   )
 }
