af63a59730d7d05b2e408441ea4de430144e2bd8
Alfonsus Ardani <alfonsusac@gmail.com>
2026-07-27 10:10:15 +0700

    nfe icons

--- src/packages/near-future-exploration.ts
+++ src/packages/near-future-exploration.ts
@@ -3,24 +3,118 @@ import type { Package } from "./types"
 export const nearfutureexpansion: Package = {
   name: "Near Future Exploration",
   antennas: {
-    "nfex-antenna-feeder-direct-1":   { label: "F-DA Direct Antenna Feed",          type: "direct", rating:            5_000, combinabilityExponent: 0.5, image: "" },
-    "nfex-antenna-rover-1":           { label: "AX-4 Pointable Helical Antenna",    type: "direct", rating:           40_000, combinabilityExponent: 0.75, image: "" },
-    "nfex-antenna-phased-single-1":   { label: "PH-1 Phased Array Antenna Element", type: "direct", rating:          125_000, combinabilityExponent: 1, image: "" },
-    "nfex-antenna-rover-2":           { label: "AX-5 Aerial Micro-Antenna",         type: "direct", rating:          150_000, combinabilityExponent: 0.75, image: "" },
-    "nfex-antenna-phased-single-2":   { label: "PH-2 Phased Array Antenna Element", type: "direct", rating:          200_000, combinabilityExponent: 1, image: "" },
-    "nfex-antenna-rover-3":           { label: "AX-30 High Gain Micro-Antenna",     type: "direct", rating:          300_000, combinabilityExponent: 0.75, image: "" },
-    "nfex-antenna-static-mini-1":     { label: "DR-1 High Gain Antenna",            type: "direct", rating:          300_000, combinabilityExponent: 0.75, image: "" },
-    "nfex-antenna-deploy-wv3-1":      { label: "DR-3 Deployable High Gain Antenna", type: "direct", rating:          500_000, combinabilityExponent: 0.75, image: "" },
-    "nfex-antenna-phased-single-3":   { label: "PH-3 Phased Array Antenna Element", type: "direct", rating:          800_000, combinabilityExponent: 1, image: "" },
-    "nfex-antenna-top-dish-1":        { label: "D-2 Spot Antenna",                  type: "direct", rating:        2_000_000, combinabilityExponent: 0.75, image: "" },
-    "nfex-antenna-top-dish-2":        { label: "D-50 Large Spot Antenna",           type: "direct", rating:       50_000_000, combinabilityExponent: 0.75, image: "" },
-    "nfex-antenna-feeder-relay-1":    { label: "F-RA Relay Antenna Feed",           type: "relay", rating:            5_000, combinabilityExponent: 0.5, image: "" },
-    "nfex-antenna-relay-tiny-1":      { label: "RA-00-2 Micro-Relay Antenna",       type: "relay", rating:        2_000_000, combinabilityExponent: 0.75, image: "" },
-    "nfex-antenna-phased-array-1":    { label: "RA-X1 Phased Relay Antenna",        type: "relay", rating:        6_000_000, combinabilityExponent: 0.25, image: "" },
-    "nfex-antenna-phased-array-2":    { label: "RA-X2 Phased Relay Antenna",        type: "relay", rating:       10_000_000, combinabilityExponent: 0.25, image: "" },
-    "nfex-antenna-relay-tdrs-1":      { label: "RA-0-8 Relay Antenna",              type: "relay", rating:       85_000_000, combinabilityExponent: 0.75, image: "" },
-    "nfex-antenna-phased-array-3":    { label: "RA-X3 Phased Relay Antenna",        type: "relay", rating:      10_0000_000, combinabilityExponent: 0.25, image: "" },
-    "nfex-antenna-relay-tdrs-2":      { label: "RA-5B Advanced Relay Antenna",      type: "relay", rating:      500_000_000, combinabilityExponent: 0.75, image: "" },
+    "nefx-ax-04": {
+      label: "C0+ AX-04 Pointable Helical Antenna",
+      image: "/nfe/antenna/ax04.png",
+      type: "direct", rating: 40_000, combinabilityExponent: 0.5,
+    },
+    "nefx-ax-05": {
+      label: "C0+ AX-05 Aerial Micro-Antenna",
+      image: "/nfe/antenna/ax05.png",
+      type: "direct", rating: 150_000, combinabilityExponent: 0.75,
+    },
+    "nefx-ph-1": {
+      label: "C0+ PH-1 Phased Array Antenna Element",
+      image: "/nfe/antenna/ph1.png",
+      type: "direct", rating: 125_000, combinabilityExponent: 1,
+    },
+    "nefx-ph-2": {
+      label: "C0+ PH-2 Phased Array Antenna Element",
+      image: "/nfe/antenna/ph2.png",
+      type: "direct", rating: 200_000, combinabilityExponent: 1,
+    },
+    "nefx-ax-30": {
+      label: "C0++ AX-30 High Gain Micro-Antenna",
+      image: "/nfe/antenna/ax30.png",
+      type: "direct", rating: 300_000, combinabilityExponent: 0.75
+    },
+    "nefx-dr01": {
+      label: "C0++ DR-1 High Gain Antenna",
+      image: "/nfe/antenna/dr01.png",
+      type: "direct", rating: 300_000, combinabilityExponent: 0.75
+    },
+    "nefx-dr-3": {
+      label: "C1 DR-3 Deployable High Gain Antenna",
+      image: "/nfe/antenna/dr3.png",
+      type: "direct", rating: 500_000, combinabilityExponent: 0.75,
+    },
+    "nefx-ph-3": {
+      label: "C1+ PH-3 Phased Array Antenna Element",
+      image: "/nfe/antenna/ph3.png",
+      type: "direct", rating: 800_000, combinabilityExponent: 1,
+    },
+    "nefx-ra-00-2": {
+      label: "C1++ RA-00-2 Micro-Relay Antenna",
+      image: "/nfe/antenna/ra002.png",
+      type: "relay", rating: 2_000_000, combinabilityExponent: 0.75
+    },
+    "nefx-d2": {
+      label: "C1++ Spot Antenna D-2",
+      image: "/nfe/antenna/d2.png",
+      type: "direct", rating: 2_000_000, combinabilityExponent: 0.75
+    },
+    "nefx-gra-x1": {
+      label: "C2+ GRA-X1 Phased Relay Antenna",
+      image: "/nfe/antenna/grax1.png",
+      type: "relay", rating: 6_000_000, combinabilityExponent: 0.25
+    },
+    "nefx-gra-x2": {
+      label: "C2+ GRA-X2 Phased Relay Antenna",
+      image: "/nfe/antenna/grax2.png",
+      type: "relay", rating: 10_000_000, combinabilityExponent: 0.25
+    },
+    "nefx-d50": {
+      label: "C2+ Large Spot Antenna D-50",
+      image: "/nfe/antenna/d50.png",
+      type: "direct", rating: 50_000_000, combinabilityExponent: 0.75
+    },
+    "nefx-ra08": {
+      label: "C2+ RA-0-8 Relay Antenna",
+      image: "/nfe/antenna/ra08.png",
+      type: "relay", rating: 85_000_000, combinabilityExponent: 0.75
+    },
+    "nefx-grax3": {
+      label: "C2++ GRA-X3 Phased Relay Antenna",
+      image: "/nfe/antenna/grax3.png",
+      type: "relay", rating: 100_000_000, combinabilityExponent: 0.25
+    },
+    "nefx-ra5b": {
+      label: "C2++ RA-5B Advanced Relay Antenna",
+      image: "/nfe/antenna/ra5b.png",
+      type: "relay", rating: 500_000_000, combinabilityExponent: 0.75
+    },
+    "nefx-fda": {
+      label: "F-DA Direct Antenna Feed",
+      image: "/nfe/antenna/fda.png",
+      type: "direct", rating: 5_000, combinabilityExponent: 0.5,
+    },
+    "nefx-fra": {
+      label: "F-RA Relay Antenna Feed",
+      image: "/nfe/antenna/fra.png",
+      type: "relay", rating: 5_000, combinabilityExponent: 0.5
+    },
+
+
+
+
+    // "nfex-antenna-feeder-direct-1":   { label: "F-DA Direct Antenna Feed",          type: "direct", rating:            5_000, combinabilityExponent: 0.5, image: "" },
+    // "nfex-antenna-rover-1":           { label: "AX-4 Pointable Helical Antenna",    type: "direct", rating:           40_000, combinabilityExponent: 0.75, image: "" },
+    // "nfex-antenna-phased-single-1":   { label: "PH-1 Phased Array Antenna Element", type: "direct", rating:          125_000, combinabilityExponent: 1, image: "" },
+    // "nfex-antenna-rover-2":           { label: "AX-5 Aerial Micro-Antenna",         type: "direct", rating:          150_000, combinabilityExponent: 0.75, image: "" },
+    // "nfex-antenna-phased-single-2":   { label: "PH-2 Phased Array Antenna Element", type: "direct", rating:          200_000, combinabilityExponent: 1, image: "" },
+    // "nfex-antenna-rover-3":           { label: "AX-30 High Gain Micro-Antenna",     type: "direct", rating:          300_000, combinabilityExponent: 0.75, image: "" },
+    // "nfex-antenna-static-mini-1":     { label: "DR-1 High Gain Antenna",            type: "direct", rating:          300_000, combinabilityExponent: 0.75, image: "" },
+    // "nfex-antenna-deploy-wv3-1":      { label: "DR-3 Deployable High Gain Antenna", type: "direct", rating:          500_000, combinabilityExponent: 0.75, image: "" },
+    // "nfex-antenna-phased-single-3":   { label: "PH-3 Phased Array Antenna Element", type: "direct", rating:          800_000, combinabilityExponent: 1, image: "" },
+    // "nfex-antenna-top-dish-1":        { label: "D-2 Spot Antenna",                  type: "direct", rating:        2_000_000, combinabilityExponent: 0.75, image: "" },
+    // "nfex-antenna-top-dish-2":        { label: "D-50 Large Spot Antenna",           type: "direct", rating:       50_000_000, combinabilityExponent: 0.75, image: "" },
+    // "nfex-antenna-feeder-relay-1":    { label: "F-RA Relay Antenna Feed",           type: "relay", rating:            5_000, combinabilityExponent: 0.5, image: "" },
+    // "nfex-antenna-relay-tiny-1":      { label: "RA-00-2 Micro-Relay Antenna",       type: "relay", rating:        2_000_000, combinabilityExponent: 0.75, image: "" },
+    // "nfex-antenna-phased-array-1":    { label: "RA-X1 Phased Relay Antenna",        type: "relay", rating:        6_000_000, combinabilityExponent: 0.25, image: "" },
+    // "nfex-antenna-phased-array-2":    { label: "RA-X2 Phased Relay Antenna",        type: "relay", rating:       10_000_000, combinabilityExponent: 0.25, image: "" },
+    // "nfex-antenna-relay-tdrs-1":      { label: "RA-0-8 Relay Antenna",              type: "relay", rating:       85_000_000, combinabilityExponent: 0.75, image: "" },
+    // "nfex-antenna-phased-array-3":    { label: "RA-X3 Phased Relay Antenna",        type: "relay", rating:      10_0000_000, combinabilityExponent: 0.25, image: "" },
+    // "nfex-antenna-relay-tdrs-2":      { label: "RA-5B Advanced Relay Antenna",      type: "relay", rating:      500_000_000, combinabilityExponent: 0.75, image: "" },
     // "nfex-antenna-reflector-side-1":  { label: "RFL-1 Dish Reflector",              type: "reflector", rating:        9_000_000, combinabilityExponent: 1, image: "" },
     // "nfex-antenna-reflector-side-2":  { label: "RFL-2 Medium Dish Reflector",       type: "reflector", rating:       75_000_000, combinabilityExponent: 1, image: "" },
     // "nfex-antenna-reflector-side-3":  { label: "RFL-3 Dish Reflector Array",        type: "reflector", rating:      850_000_000, combinabilityExponent: 1, image: "" },
--- src/packages/probesplus.ts
+++ src/packages/probesplus.ts
@@ -5,103 +5,206 @@ export const probesplus: Package = {
   dsnLevels: {},
   planets: {},
   antennas: {
-    "pp_ca_ant_gps": {
-      image: "",
-      label: "CA-KPS KerbNet Position System Antenna",
-      type: "direct",
-      rating: 400_000,
-      combinabilityExponent: 1
-    },
-    "pp_antenna_tv": {
-      image: "",
+    "ca-a01": {
       label: "CA-A01 Ground Plane Antenna",
-      type: "direct",
-      rating: 500_000,
-      combinabilityExponent: 1
+      image: "/probesplus/antenna/ca-a01.png",
+      type: "direct", rating: 500_000, combinabilityExponent: 1,
     },
-    "pp_antenna_quetzal": {
-      image: "",
-      label: "CA-A06 Quetzal Omni Antenna",
-      type: "direct",
-      rating: 500_000,
-      combinabilityExponent: 0
-    },
-    "pp_ca_landv_omni": {
-      image: "",
-      label: "CA-A07 Landvermesser Omni Antenna",
-      type: "direct",
-      rating: 505_000,
-      combinabilityExponent: 1
-    },
-    "pp_antenna_cone_toggle": {
-      image: "",
+    "ca-a02": {
       label: "CA-A02 Conic Antenna",
-      type: "direct",
-      rating: 550_000,
-      combinabilityExponent: 0
+      image: "/probesplus/antenna/ca-a02.png",
+      type: "direct", rating: 550_000, combinabilityExponent: 0,
     },
-    "pp_ca_landv_hga": {
-      image: "",
-      label: "CA-AE20 HGA Antenna and Solar Panel",
-      type: "direct",
-      rating: 1_000_000,
-      combinabilityExponent: 0.75
+    "ca-a06": {
+      label: "CA-A06 'Quetzal' Omni Antenna",
+      image: "/probesplus/antenna/ca-a06.png",
+      type: "direct", rating: 500_000, combinabilityExponent: 0,
     },
-    "pp_dish_deploy_S2": {
-      image: "",
-      label: "CA-AD1-R Small Folding Relay Antenna",
-      type: "relay",
-      rating: 2_500_000,
-      combinabilityExponent: 0.75
+    "ca-a07": {
+      label: "CA-A07 Landvermesser Omni Antenna",
+      image: "/probesplus/antenna/ca-a07.png",
+      type: "direct", rating: 505_000, combinabilityExponent: 1,
     },
-    "pp_dish_deploy_S": {
-      image: "",
+    "ca-a10": {
       label: "CA-A10 Small Folding Relay Antenna",
-      type: "relay",
-      rating: 5_000_000,
-      combinabilityExponent: 0.75
+      image: "/probesplus/antenna/ca-a10.png",
+      type: "relay", rating: 5_000_000, combinabilityExponent: 0.75,
     },
-    "pp_dish_xihe": {
-      image: "",
-      label: "CA-D02 Medium Folding Relay Antenna",
-      type: "relay",
-      rating: 10_000_000,
-      combinabilityExponent: 0.75
-    },
-    "pp_dish_S": {
-      image: "",
+    "ca-a100": {
       label: "CA-A100 Small Dish Antenna",
-      type: "direct",
-      rating: 2_500_000_000,
-      combinabilityExponent: 0.75
+      image: "/probesplus/antenna/ca-a100.png",
+      type: "direct", rating: 2_500_000_000, combinabilityExponent: 0.75
     },
-    "pp_dish_tatsujin": {
-      image: "",
+    "ca-a180": {
       label: "CA-A180 Tatsujin Relay Antenna",
-      type: "relay",
-      rating: 15_000_000_000,
-      combinabilityExponent: 0.75
+      image: "/probesplus/antenna/ca-a180.png",
+      type: "relay", rating: 15_000_000_000, combinabilityExponent: 0.75,
     },
-    "pp_dish_hera": {
-      image: "",
+    "ca-a190": {
       label: "CA-A190 Hera Dish Antenna",
-      type: "relay",
-      rating: 30_000_000_000,
-      combinabilityExponent: 0.75
+      image: "/probesplus/antenna/ca-a190.png",
+      type: "relay", rating: 30_000_000_000, combinabilityExponent: 0.75,
     },
-    "pp_dish_quetzal": {
-      image: "",
+    "ca-a200": {
       label: "CA-A200 Quetzal Relay Antenna",
-      type: "relay",
-      rating: 50_000_000_000,
-      combinabilityExponent: 0.75
+      image: "/probesplus/antenna/ca-a200.png",
+      type: "relay", rating: 50_000_000_000, combinabilityExponent: 0.75,
     },
-    "pp_dish_L": {
-      image: "",
+    "ca-a20-b": {
+      label: "CA-A20-B HGA Antenna",
+      image: "/probesplus/antenna/ca-a20-b.png",
+      type: "direct", rating: 1_000_000, combinabilityExponent: 0.75,
+    },
+    "ca-a300": {
       label: "CA-A300 Torekka Relay Antenna",
-      type: "relay",
-      rating: 100_000_000_000,
-      combinabilityExponent: 0.75
+      image: "/probesplus/antenna/ca-a300.png",
+      type: "relay", rating: 100_000_000_000, combinabilityExponent: 0.75,
     },
+    "ca-ad1-r": {
+      label: "CA-AD1-R Small Folding Relay Antenna",
+      image: "/probesplus/antenna/ca-ad1-r.png",
+      type: "relay", rating: 2_500_000, combinabilityExponent: 0.75,
+    },
+    "ca-ae20": {
+      label: "CA-AE20 HGA Antenna and Solar Panel",
+      image: "/probesplus/antenna/ca-ae20.png",
+      type: "direct", rating: 1_000_000, combinabilityExponent: 0.75,
+    },
+    "ca-d02": {
+      label: "CA-D02 Medium Folding Relay Antenna",
+      image: "/probesplus/antenna/ca-d02.png",
+      type: "relay", rating: 10_000_000, combinabilityExponent: 0.75,
+    },
+    "cae-102": {
+      label: "CAE-102 Vorona Dish Antenna",
+      image: "/probesplus/antenna/cae-102.png",
+      type: "relay", rating: 3_000_000_000, combinabilityExponent:0.75,
+    },
+    "cae-a03": {
+      label: "CAE-A03 Vorona Communication Array",
+      image: "/probesplus/antenna/cae-a03.png",
+      type: "direct", rating: 960_000, combinabilityExponent: 0,
+    },
+    "ca-kps": {
+      label: "CA-KPS Kerbnet Position System Antenna",
+      image: "/probesplus/antenna/ca-kps.png",
+      type: "direct", rating: 400_000, combinabilityExponent: 1,
+    },
+    "ca-mer-a400": {
+      label: "CA-MER-A400 Meridiani Dish Antenna",
+      image: "/probesplus/antenna/ca-mer-a400.png",
+      type: "relay", rating: 100_000_000_000, combinabilityExponent: 0.75,
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // "pp_ca_ant_gps": {
+    //   image: "",
+    //   label: "CA-KPS KerbNet Position System Antenna",
+    //   type: "direct",
+    //   rating: 400_000,
+    //   combinabilityExponent: 1
+    // },
+    // "pp_antenna_tv": {
+    //   image: "",
+    //   label: "CA-A01 Ground Plane Antenna",
+    //   type: "direct",
+    //   rating: 500_000,
+    //   combinabilityExponent: 1
+    // },
+    // "pp_antenna_quetzal": {
+    //   image: "",
+    //   label: "CA-A06 Quetzal Omni Antenna",
+    //   type: "direct",
+    //   rating: 500_000,
+    //   combinabilityExponent: 0
+    // },
+    // "pp_ca_landv_omni": {
+    //   image: "",
+    //   label: "CA-A07 Landvermesser Omni Antenna",
+    //   type: "direct",
+    //   rating: 505_000,
+    //   combinabilityExponent: 1
+    // },
+    // "pp_antenna_cone_toggle": {
+    //   image: "",
+    //   label: "CA-A02 Conic Antenna",
+    //   type: "direct",
+    //   rating: 550_000,
+    //   combinabilityExponent: 0
+    // },
+    // "pp_ca_landv_hga": {
+    //   image: "",
+    //   label: "CA-AE20 HGA Antenna and Solar Panel",
+    //   type: "direct",
+    //   rating: 1_000_000,
+    //   combinabilityExponent: 0.75
+    // },
+    // "pp_dish_deploy_S2": {
+    //   image: "",
+    //   label: "CA-AD1-R Small Folding Relay Antenna",
+    //   type: "relay",
+    //   rating: 2_500_000,
+    //   combinabilityExponent: 0.75
+    // },
+    // "pp_dish_deploy_S": {
+    //   image: "",
+    //   label: "CA-A10 Small Folding Relay Antenna",
+    //   type: "relay",
+    //   rating: 5_000_000,
+    //   combinabilityExponent: 0.75
+    // },
+    // "pp_dish_xihe": {
+    //   image: "",
+    //   label: "CA-D02 Medium Folding Relay Antenna",
+    //   type: "relay",
+    //   rating: 10_000_000,
+    //   combinabilityExponent: 0.75
+    // },
+    // "pp_dish_S": {
+    //   image: "",
+    //   label: "CA-A100 Small Dish Antenna",
+    //   type: "direct",
+    //   rating: 2_500_000_000,
+    //   combinabilityExponent: 0.75
+    // },
+    // "pp_dish_tatsujin": {
+    //   image: "",
+    //   label: "CA-A180 Tatsujin Relay Antenna",
+    //   type: "relay",
+    //   rating: 15_000_000_000,
+    //   combinabilityExponent: 0.75
+    // },
+    // "pp_dish_hera": {
+    //   image: "",
+    //   label: "CA-A190 Hera Dish Antenna",
+    //   type: "relay",
+    //   rating: 30_000_000_000,
+    //   combinabilityExponent: 0.75
+    // },
+    // "pp_dish_quetzal": {
+    //   image: "",
+    //   label: "CA-A200 Quetzal Relay Antenna",
+    //   type: "relay",
+    //   rating: 50_000_000_000,
+    //   combinabilityExponent: 0.75
+    // },
+    // "pp_dish_L": {
+    //   image: "",
+    //   label: "CA-A300 Torekka Relay Antenna",
+    //   type: "relay",
+    //   rating: 100_000_000_000,
+    //   combinabilityExponent: 0.75
+    // },
   },
 }
