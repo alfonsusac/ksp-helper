160693b76d4bdf57834bc73d810d96df190d553c
Alfonsus Ardani <alfonsusac@gmail.com>
2026-08-16 00:49:39 +0700

    instrumetntation

---
+++ src/instrumentation.ts
@@ -0,0 +1,28 @@
+import type { Instrumentation } from "next"
+
+export const onRequestError: Instrumentation.onRequestError = async (
+  err,
+  request,
+  context
+) => {
+  const message = err instanceof Error ? err.message : String(err)
+  const digest =
+    typeof err === 'object' && err !== null && 'digest' in err
+      ? String(err.digest)
+      : undefined
+
+  await fetch(process.env[ 'ALFONDEV_DISCORD_SERVER_LOG_WEBHOOK' ]!, {
+    method: 'POST',
+    body: JSON.stringify({
+      thread_id: '1538230599423098911',
+      content: {
+        request,
+        context
+      },
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
+
