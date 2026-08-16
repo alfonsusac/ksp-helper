4fc3c1b63aa0cc36242037d8e9762a930f5b8f0d
Alfonsus Ardani <alfonsusac@gmail.com>
2026-08-17 00:37:50 +0700

    remove coment

--- src/app/spacedock/_components/mod-item-card.tsx
+++ src/app/spacedock/_components/mod-item-card.tsx
@@ -103,11 +103,3 @@ export function ModItemCard(props: {
     </div>
   )
 }
-
-const rtf = new Intl.RelativeTimeFormat("en", {
-  localeMatcher: "best fit", // other values: "lookup"
-  numeric: "always", // other values: "auto"
-  style: "long", // other values: "short" or "narrow"
-})
-
-// rtf.format()
